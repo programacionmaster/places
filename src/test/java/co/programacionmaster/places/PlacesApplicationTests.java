b37af5dafa5ed09871423383b628884e54fcef22
barcochrist <barcochrist@gmail.com>
2021-07-15 19:52:27 -0500

    Add basic structure for Spring Boot application

---
+++ src/test/java/co/programacionmaster/places/PlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.programacionmaster.places;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
